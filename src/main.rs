5a29af7f50e0038fd11b3d327cd7b863dc173dcd
POCHI-CUTE <takaono1789@gmail.com>
2023-10-08 13:58:51 +0900

    css dom

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-pub mod html;
-pub mod dom;
+mod html;
+mod dom;
+mod css;
 
 fn main() {
     let htmlStr = "<html><body>Hello, world!</body></html>".to_string();
     let dom = html::parse(htmlStr);
 
     println!("{:?}", dom);
+
+    let cssStr = String::from("body { background: red; }");
+    let stylesheet = css::parse(cssStr);
+    println!("{:?}", stylesheet)
 }
